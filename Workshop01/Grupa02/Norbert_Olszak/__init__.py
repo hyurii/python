9e88978dee8c9cae51f7c0172b5cecc0f917545b
olszanor <norbert.olszak@brightone.com>
2016-07-04 10:35:20 +0200

    Zadanie z pr

---
+++ Workshop01/Grupa02/Norbert_Olszak/__init__.py
@@ -0,0 +1,31 @@
+'''
+
+@author: OlszakNorbert
+
+'''
+from math import pow, sqrt
+from doctest import REPORT_CDIFF
+
+'''
+pow() - potêgowanie
+sqrt() - dzielenie
+print - koñcowy obiekt
+
+
+'''
+print 
+
+'''
+Podaj iloœæ prek wszystkich
+
+FAK A=
+    B=
+    C=
+    
+    procent wszystkich prkek print ABC
+    
+    
+    zainstalowaæ REPO
+    
+    PY20
+'''
