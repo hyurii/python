e4ffffdb1abdcace2dd8ebe35a9255efc6495b97
ephmoc <ephmoc@gmail.com>
2016-07-05 22:24:31 +0200

    bubble sort

--- Workshop01/Grupa02/krzysztof_hubaczek/bubbleSort.py
+++ Workshop01/Grupa02/krzysztof_hubaczek/bubbleSort.py
@@ -7,6 +7,24 @@ def bubbleSort(lista):
                 lista[i + 1] = temp
 
 
+def shortBubbleSort(lista):
+    zamiana = True
+    raszta = len(lista) - 1
+    while raszta > 0 and zamiana:
+        zamiana = False
+        for i in range(raszta):
+            if lista[i] > lista[i + 1]:
+                zamiana = True
+                temp = lista[i]
+                lista[i] = lista[i + 1]
+                lista[i + 1] = temp
+        raszta -= 1
+
+
 lista = [4, 6, 7, 1]
 bubbleSort(lista)
-print lista
+print(lista)
+
+lista = [4, 6, 7, 1]
+shortBubbleSort(lista)
+print(lista)
