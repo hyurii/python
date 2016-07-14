a3c044ba940889bb8687895c9ac90f5a5a940f17
Zablodan <zablocki92@gmail.com>
2016-07-14 13:10:22 +0200

    funkcje

--- Workshop01/Grupa01/kalkulator/funkcje.py
+++ Workshop01/Grupa01/kalkulator/funkcje.py
@@ -38,10 +38,29 @@ def pierwiastek():
     return y
 
 
-y = pierwiastek()
-print y
+def odejmowanie():
+    
+    liczba_A = inputInt("Podaj liczbe bazowa: ")
+    liczba_B = inputInt("Podaj liczbe odejmowana: ")
+    
+    wynik = liczba_A - liczba_B
+    return wynik
+    # test return str(liczba_A) + " - " + str(liczba_B) + " = " + str(wynik)
+    
+    
+def dzielenie():
+    liczba_A = inputInt("Podaj dzielnik: ")
+    liczba_B = inputInt("Podaj dzielna: ")
+   
+    wynik = liczba_A / liczba_B
+    return wynik
+ #   return str(liczba_A) + " / " + str(liczba_B) + " = " + str(wynik)
 
-y = dodaj()
-print y
-y = pomnoz()
-print y
+def potega():
+    liczba_A = inputInt("Podaj liczbe bazowa: ")
+    liczba_B = inputInt("Podnosze do potegi: ")
+  
+    wynik = liczba_A ** liczba_B
+    
+   # return str(liczba_A) + " ** " + str(liczba_B) + " = " + str(wynik)
+    return wynik
