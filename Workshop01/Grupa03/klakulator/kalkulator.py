aea71283d78e75664a9cc497261d5c252e0b6d8d
Shaomie <shaomie@gmail.com>
2016-07-15 13:06:38 +0200

    kalkus

--- Workshop01/Grupa03/klakulator/kalkulator.py
+++ Workshop01/Grupa03/klakulator/kalkulator.py
@@ -5,4 +5,27 @@ Created on 15 lip 2016
 '''
 
 if __name__ == '__main__':
+    
+    print "Dostepne dzialania: "
+    print "[P] dodawanie "
+    print "[O] odejmowanie "
+    print "[M] mnozenie "
+    print "[D] dzielenie"
+    print "[W] pierwiastek "
+    print "[T] potega"
+    
+    Zmienna=raw_input("Podaj dzialanie jakie chcesz wykonac: ")
+    if Zmienna == "P":
+        suma()
+    elif Zmienna =="O":
+        odejm()
+    elif Zmienna=="M":
+        mnoz()
+    elif Zmienna=="D":
+        dziel()
+    elif Zmienna == "W":
+        pierw()
+    elif Zmienna == "T":
+        potega()
+    
     pass
