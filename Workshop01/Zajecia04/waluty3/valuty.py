e4c35d134b1018deb9450a581f40aacbe5ece759
ephmoc <ephmoc@gmail.com>
2016-07-29 12:35:47 +0200

    pliki

--- Workshop01/Zajecia04/waluty3/valuty.py
+++ Workshop01/Zajecia04/waluty3/valuty.py
@@ -3,6 +3,16 @@ Created on 29 lip 2016
 
 @author: giberkrz
 '''
+import pickle
+
+dict = {"CHF": 2.33, "PLN": 4.55, "GBP": 5.66}
+
+
+def writeFile(x):
+    plik = open("plik.txt", "w")
+    pickle.dump(x, plik, pickle.HIGHEST_PROTOCOL)
+    plik.close()
+
 
 if __name__ == '__main__':
-    pass
+    writeFile(dict)
